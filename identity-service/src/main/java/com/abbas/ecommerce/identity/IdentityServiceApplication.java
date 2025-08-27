31da44d728ee3fbb09e33b012b3033e5abb71daa
abbscbn <abbscbn@gmail.com>
2025-08-27 08:39:13 +0300

    github entegrasyonu

--- identity-service/src/main/java/com/abbas/ecommerce/identity/IdentityServiceApplication.java
+++ identity-service/src/main/java/com/abbas/ecommerce/identity/IdentityServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IdentityServiceApplication {
-
+//yorum satırı
 	public static void main(String[] args) {
 		SpringApplication.run(IdentityServiceApplication.class, args);
 	}
