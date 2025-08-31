aea1c33fecd04e2ed3ab81a9a7d3c792f3869ba1
abbscbn <abbscbn@gmail.com>
2025-08-31 08:56:39 +0300

    Common lib modülü gerekli bir şekilde güncellendi

--- common-lib/src/main/java/com/abbas/ecommerce/common/event/OrderCreatedEvent.java
+++ common-lib/src/main/java/com/abbas/ecommerce/common/event/OrderCreatedEvent.java
@@ -1,5 +1,25 @@
 package com.abbas.ecommerce.common.event;
 
-import com.abbas.ecommerce.common.dto.OrderDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-public record OrderCreatedEvent(OrderDTO order) {}
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderCreatedEvent{
+    private Long orderId;
+    private Long userId;
+    private List<OrderItemDto> items; // productId, quantity
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class OrderItemDto{
+        private Long productId;
+        private Integer quantity;
+    }
+}
